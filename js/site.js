719302539e84a4edc21deac70db6495e65462ed1
Adam Berry <aberry2288@gmail.com>
2023-07-25 14:20:12 -0400

    updated the code page

--- js/site.js
+++ js/site.js
@@ -1,13 +1,35 @@
-function displayMessage() {
-    let inputBox = document.getElementById('message');
+function getValues() {
 
-    let message = inputBox.value;
+    let userString = document.getElementById('userString').value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    );
+    let revString = reverseString(userString);
+
+    displayString(revString);
+
+}
+
+function reverseString(input) {
+
+   let revString = '';
+
+    for (let index = input.length - 1; index >= 0; index = index - 1) {
+
+        let letter = input[index];
+
+        revString = revString + letter;
+    }
+
+    
+
+    return revString;
+}
+
+function displayString(output) {
+
+    document.getElementById('results').textContent = output;
+
+    let alertBox = document.getElementById('alert');
+
+    alertBox.classList.remove('invisible');
+
 }
